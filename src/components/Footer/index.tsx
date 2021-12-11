4a93a29fca4377e56f1c9ad1891809d81709cb96
Kianoosh Hooshmand <kianoosh.hooshmand@gmail.com>
2021-12-11 08:12:42 +0100

    feat: added Vazir font, added a new logo, fixed the footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <div className="fixed left-0 bottom-0 w-full text-center">
+      <p>❤️ Created with </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,18 +1,19 @@
 import { FC } from "react";
 import { Outlet } from "react-router";
+import Footer from "components/Footer";
 
 const AuthLayout: FC = (): JSX.Element => {
   return (
     <div className="h-screen bg-gray-100 flex flex-col justify-center items-center">
       <div className="flex flex-col items-center">
-        <img src="/icons/logo.svg" alt="rockbooks-logo" className="w-80" />
+        <img
+          src="/icons/logo.svg"
+          alt="rockbooks-logo"
+          className="w-52 mb-16"
+        />
         <Outlet />
       </div>
-
-      <div>
-        <div className="mt-20" />
-        <p>Rock all the books in the world with RockBooks</p>
-      </div>
+      <Footer />
     </div>
   );
 };

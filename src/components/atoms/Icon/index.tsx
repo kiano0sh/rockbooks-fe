e2f1d800aa28511aa58b871968bb7784ae66ced7
Kianoosh Hooshmand <kianoosh.hooshmand@gmail.com>
2022-01-02 11:28:06 +0100

    feat: made the Icon clickable

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { FC } from "react";
 
 type IconNamesType = "mdArrowBack" | "mdArrowForward";
@@ -5,15 +6,28 @@ type IconNamesType = "mdArrowBack" | "mdArrowForward";
 interface IIcon extends IClassName {
   name: IconNamesType;
   onClick?: () => void;
+  disabled?: boolean;
+  clickable?: boolean;
 }
 
-const Icon: FC<IIcon> = ({ name, onClick, className }) => {
+const Icon: FC<IIcon> = ({
+  name,
+  onClick,
+  className,
+  clickable = false,
+  disabled = false,
+}) => {
+  const disabledClassNames = classNames({
+    "cursor-not-allowed": disabled,
+    "cursor-pointer": !disabled && clickable,
+  });
+
   return (
     <img
       src={`/icons/${name}.svg`}
       alt={name}
-      className={className}
-      onClick={onClick}
+      className={`${disabledClassNames} ${className}`}
+      onClick={!disabled ? onClick : undefined}
     />
   );
 };

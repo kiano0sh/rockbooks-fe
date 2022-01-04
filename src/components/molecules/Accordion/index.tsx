e93a9e6ce4e77c695e6aaaa224b726852b7db0cc
Kianoosh Hooshmand <kianoosh.hooshmand@gmail.com>
2022-01-04 20:19:30 +0100

    fix: improved the Accordion

--- src/components/molecules/Accordion/index.tsx
+++ src/components/molecules/Accordion/index.tsx
@@ -11,21 +11,25 @@ const Accordion: FC<IAccordion> = ({ title, className = "", children }) => {
     "hidden"
   );
   return (
-    <>
+    <div className={`w-full ${className}`}>
       <Button
         onClick={() => {
           setContentDisplay((prevState) =>
             prevState === "hidden" ? "block" : "hidden"
           );
         }}
-        name={title}
+        name={
+          <p>
+            صوت از <strong>{title}</strong>
+          </p>
+        }
         kind="positive"
-        className="w-full transition outline-none p-4"
+        className="w-96 transition outline-none p-4 h-12 text-right mb-1"
       />
-      <Card className={`px-4 hidden overflow-hidden ${contentDisplay}`}>
+      <Card className={`px-4 w-96 overflow-hidden mb-1 ${contentDisplay}`}>
         {children}
       </Card>
-    </>
+    </div>
   );
 };
 
